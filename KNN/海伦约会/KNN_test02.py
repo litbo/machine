424582f517e8e992013ba9533760f6c7576815b7
litbo <973046515@qq.com>
2020-06-04 20:57:10 +0800

    KNN测试集-训练集

--- KNN/海伦约会/KNN_test02.py
+++ KNN/海伦约会/KNN_test02.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib.font_manager import FontProperties
 import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
+import operator
 
 
 def file2matrix(filename):
@@ -92,31 +93,113 @@ def showdatas(datingDataMat, datingLabels):
     axs[1][0].legend(handles=[didntLike, smallDoses, largeDoses])
     # 显示图片
     plt.show()
+
+
 #     为了使各个数据项的权值相同，所以要对数据项进行数据归一化
 #   算法：newValue = (oldValue - min) / (max - min)
 def autoNorm(dataSet):
-    #获得数据的最小值
+    # 获得数据的最小值
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
-    #最大值和最小值的范围
+    # 最大值和最小值的范围
     ranges = maxVals - minVals
-    #shape(dataSet)返回dataSet的矩阵行列数
+    # shape(dataSet)返回dataSet的矩阵行列数
     normDataSet = np.zeros(np.shape(dataSet))
-    #返回dataSet的行数
+    # 返回dataSet的行数
     m = dataSet.shape[0]
-    #原始值减去最小值
+    # 原始值减去最小值
     normDataSet = dataSet - np.tile(minVals, (m, 1))
-    #除以最大和最小值的差,得到归一化数据
+    # 除以最大和最小值的差,得到归一化数据
     normDataSet = normDataSet / np.tile(ranges, (m, 1))
-    #返回归一化数据结果,数据范围,最小值
+    # 返回归一化数据结果,数据范围,最小值
     return normDataSet, ranges, minVals
 
 
-if __name__ == '__main__':
+def datingClassTest():
+    # 打开的文件名
     filename = "datingTestSet.txt"
-    # 打开并处理数据
+    # 将返回的特征矩阵和分类向量分别存储到datingDataMat和datingLabels中
     datingDataMat, datingLabels = file2matrix(filename)
-    print(datingDataMat)
-    normDataSet, ranges, minVals = autoNorm(datingDataMat)
-    print(normDataSet)
-    showdatas(datingDataMat,datingLabels)
+    # 取所有数据的百分之十
+    hoRatio = 0.10
+    # 数据归一化,返回归一化后的矩阵,数据范围,数据最小值
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # 获得normMat的行数
+    m = normMat.shape[0]
+    # 百分之十的测试数据的个数
+    numTestVecs = int(m * hoRatio)
+    # 分类错误计数
+    errorCount = 0.0
+
+    for i in range(numTestVecs):
+        # 前numTestVecs个数据作为测试集,后m-numTestVecs个数据作为训练集
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
+                                     datingLabels[numTestVecs:m], 4)
+        print("分类结果:%d\t真实类别:%d" % (classifierResult, datingLabels[i]))
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+    print("错误率:%f%%" % (errorCount / float(numTestVecs) * 100))
+
+
+def classify0(inX, dataSet, labels, k):
+
+    # numpy函数shape[0]返回dataSet的行数
+    dataSetSize = dataSet.shape[0]
+    # 在列向量方向上重复inX共1次(横向),行向量方向上重复inX共dataSetSize次(纵向)
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    # 二维特征相减后平方
+    sqDiffMat = diffMat ** 2
+    # sum()所有元素相加,sum(0)列相加,sum(1)行相加
+    sqDistances = sqDiffMat.sum(axis=1)
+    # 开方,计算出距离
+    distances = sqDistances ** 0.5
+    # 返回distances中元素从小到大排序后的索引值
+    sortedDistIndices = distances.argsort()
+    # 定一个记录类别次数的字典
+    classCount = {}
+    for i in range(k):
+        # 取出前k个元素的类别
+        voteIlabel = labels[sortedDistIndices[i]]
+        # dict.get(key,default=None),字典的get()方法,返回指定键的值,如果值不在字典中返回默认值。
+        # 计算类别次数
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    # key=operator.itemgetter(1)根据字典的值进行排序
+    # key=operator.itemgetter(0)根据字典的键进行排序
+    # reverse降序排序字典
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    print(sortedClassCount)
+    # 返回次数最多的类别,即所要分类的类别
+    return sortedClassCount[0][0]
+
+def classifyPerson():
+    #输出结果
+    resultList = ['讨厌','有些喜欢','非常喜欢']
+    #三维特征用户输入
+    ffMiles = float(input("每年获得的飞行常客里程数:"))
+    precentTats = float(input("玩视频游戏所耗时间百分比:"))
+    iceCream = float(input("每周消费的冰激淋公升数:"))
+    #打开的文件名
+    filename = "datingTestSet.txt"
+    #打开并处理数据
+    datingDataMat, datingLabels = file2matrix(filename)
+    #训练集归一化
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    #生成NumPy数组,测试集
+    inArr = np.array([ffMiles,precentTats,  iceCream])
+    #测试集归一化
+    norminArr = (inArr - minVals) / ranges
+    #返回分类结果
+    classifierResult = classify0(norminArr, normMat, datingLabels, 50)
+    #打印结果
+    print("你可能%s这个人" % (resultList[classifierResult-1]))
+
+if __name__ == '__main__':
+    # filename = "datingTestSet.txt"
+    # # 打开并处理数据
+    # datingDataMat, datingLabels = file2matrix(filename)
+    # print(datingDataMat)
+    # normDataSet, ranges, minVals = autoNorm(datingDataMat)
+    # print(normDataSet)
+    # showdatas(datingDataMat,datingLabels)
+    # datingClassTest()
+    classifyPerson()
